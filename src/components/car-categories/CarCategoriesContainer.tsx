d97d2100dad42745579f5aca62828113c28a186c
HITimran <HitImran@gmail.com>
2024-10-20 20:31:17 -0700

    loading component

--- src/components/car-categories/CarCategoriesContainer.tsx
+++ src/components/car-categories/CarCategoriesContainer.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import CarCategoriesGrid from './CarCategoriesGrid';
+import Loading from '../common/Loading';
+import Error from '../common/Error';
 import { useCarData } from '../../hooks/useCarData';
+import { useError } from '../../hooks/useError';
 
 const CarCategoriesContainer: React.FC = () => {
-  const { categories, carTypes, isLoading, error } = useCarData();
+  const { categories, carTypes, isLoading } = useCarData();
+  const { error, showError } = useError();
+
+  React.useEffect(() => {
+    if (!categories.length && !carTypes.length && !isLoading) {
+      showError('Failed to load car data. Please try again later.');
+    }
+  }, [categories, carTypes, isLoading]);
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <Loading />;
   }
 
   if (error) {
-    return <div>Error: {error}</div>;
+    return <Error message={error} />;
   }
 
   return <CarCategoriesGrid categories={categories} carTypes={carTypes} />;
